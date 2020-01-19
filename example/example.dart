037d4af6f75f2e55bfc2fff1972bac37ef6315d6
Luiz Eduardo <luizeof@gmail.com>
2020-01-19 10:11:48 -0300

    Update example.dart

--- example/example.dart
+++ example/example.dart
@@ -4,4 +4,16 @@ void main() async {
   var api = await DockerAPI.connect('http://server:8099/', 'user', 'password');
 
   print(api.kernelVersion);
+  print(api.imagesDiskUsageBytes);
+  print(api.imagesDiskUsageKB);
+  print(api.imagesDiskUsageMB);
+  print(api.imagesDiskUsageGB);
+  print(api.containersDiskUsageBytes);
+  print(api.containersDiskUsageKB);
+  print(api.containersDiskUsageMB);
+  print(api.containersDiskUsageGB);
+  print(api.volumesDiskUsageBytes);
+  print(api.volumesDiskUsageKB);
+  print(api.volumesDiskUsageMB);
+  print(api.volumesDiskUsageGB);
 }
